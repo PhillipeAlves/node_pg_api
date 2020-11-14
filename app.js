9aff55693bd7c8ecdd19cb216059ec612d3ac443
PhillipeAlves <phillipe.alves@gmail.com>
2020-11-14 16:08:40 +1100

    add controllers

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const { readData, createData, updateData, deleteData } = require('./routes/api')
-const PORT = process.env.PORT
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const {
+  createData, //  C
+  readData, //  R
+  updateData, //  U
+  deleteData, //  D
+} = require("./controllers/apiController");
+const PORT = process.env.PORT;
 
 // ===( MIDDLEWARE )=== //
-
-app.use(cors())
-app.use(bodyParser.json())
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // ===( API )=== //
-
 app
-    .route('/api/data')
-    .get(readData)
-    .post(createData)
-    .put(updateData)
-    .delete(deleteData)
+  .route("/api/data")
+  .get(readData)
+  .post(createData)
+  .put(updateData)
+  .delete(deleteData);
 
 // ===( PORT )=== //
-
 app.listen(
-    PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
